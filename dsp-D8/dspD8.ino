8d67705b4edf85fd9591a0e680ffcc174946dde0
Kiki Lamb <katherinem13@Kikis-iMac.local>
2019-04-10 22:55:42 -0400

    I2C control instead for dspD8.

--- dsp-D8/dspD8.ino
+++ dsp-D8/dspD8.ino
@@ -1,10 +1,10 @@
-// dsp-D8 Drum Chip (c) DSP Synthesizers 2015
-// Free for non commercial use
-
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include "drum_samples.h"
+#include <Wire.h>
+
+#define SLAVE_ADDRESS 0x60
 
 #ifndef cbi
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
@@ -13,7 +13,6 @@
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif 
 
-// Standard Arduino Pins
 #define digitalPinToPortReg(P) \
 	(((P) >= 0 && (P) <= 7) ? &PORTD : (((P) >= 8 && (P) <= 13) ? &PORTB : &PORTC))
 #define digitalPinToDDRReg(P) \
@@ -22,38 +21,49 @@
 	(((P) >= 0 && (P) <= 7) ? &PIND : (((P) >= 8 && (P) <= 13) ? &PINB : &PINC))
 #define digitalPinToBit(P) \
 	(((P) >= 0 && (P) <= 7) ? (P) : (((P) >= 8 && (P) <= 13) ? (P) - 8 : (P) - 14))
-
 #define digitalReadFast(P) bitRead(*digitalPinToPINReg(P), digitalPinToBit(P))
-
 #define digitalWriteFast(P, V) bitWrite(*digitalPinToPortReg(P), digitalPinToBit(P), (V))
 
+#define  REG_MAP_SIZE     14
+#define  MAX_SENT_BYTES   2
+
 const unsigned char PS_128 = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
-
-
-//--------- Ringbuf parameters ----------
+byte receivedCommands[MAX_SENT_BYTES];
 uint8_t Ringbuffer[256];
 uint8_t RingWrite=0;
 uint8_t RingRead=0;
 volatile uint8_t RingCount=0;
-
-//-----------------------------------------
+uint8_t phaccBD,phaccCH,phaccCL,phaccCR,phaccOH,phaccRD,phaccRS,phaccSD;
+uint8_t pitchBD=128;
+uint8_t pitchCH=64;
+uint8_t pitchCL=64;
+uint8_t pitchCR=16;
+uint8_t pitchOH=64;
+uint8_t pitchRD=16;
+uint8_t pitchRS=64;
+uint8_t pitchSD=64;
+uint16_t samplecntBD,samplecntCH,samplecntCL,samplecntCR,samplecntOH,samplecntRD,samplecntRS,samplecntSD;
+uint16_t samplepntBD,samplepntCH,samplepntCL,samplepntCR,samplepntOH,samplepntRD,samplepntRS,samplepntSD;
+uint8_t oldPORTB;
+uint8_t oldPORTD;
+int16_t total;
+volatile bool dataReady = false;
+byte current_triggers = 0;
+byte next_triggers = 0;
+bool flip = false;
+byte next_pitches[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+  
 ISR(TIMER1_COMPA_vect) {
-
-	//-------------------  Ringbuffer handler -------------------------
-
 	if (RingCount) {                            //If entry in FIFO..
 		OCR2A = Ringbuffer[(RingRead++)];          //Output LSB of 16-bit DAC
 		RingCount--;
 	}
-
-	//-----------------------------------------------------------------
-
 }
 
-
-
-void setup() 
-{
+void setup() { 
+  receivedCommands[0] = 0;
+  receivedCommands[1] = 0;
+  
 	OSCCAL=0xFF;
 
 	//Drumtrigger inputs
@@ -87,7 +97,6 @@ void setup()
 	OCR1A = 400; //40KHz Samplefreq
 
 	// Set up Timer 2 to do pulse width modulation on D11
-
 	// Use internal clock (datasheet p.160)
 	ASSR &= ~(_BV(EXCLK) | _BV(AS2));
 
@@ -119,184 +128,200 @@ void setup()
 	TIMSK0=0;
 
 
-	// set up the ADC
-	ADCSRA &= ~PS_128;  // remove bits set by Arduino library
-	// Choose prescaler PS_128.
-	ADCSRA |= PS_128;
-	ADMUX = 64;
-	sbi(ADCSRA, ADSC);
+//	// set up the ADC
+//	ADCSRA &= ~PS_128;  // remove bits set by Arduino library
+//	// Choose prescaler PS_128.
+//	ADCSRA |= PS_128;
+//	ADu = 64;
+//	sbi(ADCSRA, ADSC);
 
-	Serial.begin(9600);
+	Serial.begin(115200);
 	Serial.println("Modified [Jan Ostman] dsp-D8:");
 	Serial.println("https://janostman.wordpress.com/the-dsp-d8-drum-chip-source-code/");
-	Serial.println("Press asdfjkl; to play the drums.");
+
+  Wire.begin(SLAVE_ADDRESS); 
+  Wire.onReceive(receiveEvent);
+  
+  pinMode(2, INPUT);
+  attachInterrupt(digitalPinToInterrupt(2), updateParams, CHANGE);
 
 	sei();
 }
 
-uint8_t phaccBD,phaccCH,phaccCL,phaccCR,phaccOH,phaccRD,phaccRS,phaccSD;
-uint8_t pitchBD=128;
-uint8_t pitchCH=64;
-uint8_t pitchCL=64;
-uint8_t pitchCR=16;
-uint8_t pitchOH=64;
-uint8_t pitchRD=16;
-uint8_t pitchRS=64;
-uint8_t pitchSD=64;
-uint16_t samplecntBD,samplecntCH,samplecntCL,samplecntCR,samplecntOH,samplecntRD,samplecntRS,samplecntSD;
-uint16_t samplepntBD,samplepntCH,samplepntCL,samplepntCR,samplepntOH,samplepntRD,samplepntRS,samplepntSD;
+void receiveEvent(int bytesReceived) {
+  for (int a = 0; a < bytesReceived; a++)
+    if ( a < MAX_SENT_BYTES)
+      receivedCommands[a] = Wire.read();
+    else
+      Wire.read();  // if we receive more data then allowed just throw it away
 
-uint8_t oldPORTB;
-uint8_t oldPORTD;
+  dataReady = true;
+}
 
-int16_t total;
-uint8_t divider;
-uint8_t MUX=0;
-char ser=' ';
+void updateParams() {
+  pitchBD = next_pitches[0];
+  pitchSD = next_pitches[1];
+  pitchCH = next_pitches[2];
+  pitchOH = next_pitches[3];
+  pitchRS = next_pitches[4];
+  pitchCL = next_pitches[5];
+  pitchRD = next_pitches[6];
+  pitchCR = next_pitches[7];
 
-void loop() 
-{  
+  if (next_triggers) {      
+    current_triggers = next_triggers;
+    next_triggers = 0;
+    
+    if (current_triggers & 0b10000000) {
+      samplepntBD=0;
+      samplecntBD=2154;
+    }
+    if (current_triggers & 0b01000000) {
+      samplepntSD=0;
+      samplecntSD=3482;
+    }
+    if (current_triggers & 0b00100000) {
+      samplepntCH=0;
+      samplecntCH=482;
+    }
+    if (current_triggers & 0b00010000) {
+      samplepntOH=0;
+      samplecntOH=2572;
+    }
+    if (current_triggers & 0b00001000) {
+      samplepntRS=0;
+      samplecntRS=1160;
+    }
+    if (current_triggers & 0b00000100) {
+      samplepntCL=0;
+      samplecntCL=2384;
+    }
+    if (current_triggers & 0b00000010) {
+      samplepntRD=0;
+      samplecntRD=5066;
+    }
+    if (current_triggers & 0b00000001) {
+      samplepntCR=0;
+      samplecntCR=5414;
+    }
+  }
+}
 
+void process_i2c_data() {
+  if (dataReady) {    
+    if (receivedCommands[0] & 0b10000000)      // 0b01xxxxxA 0b01234567  trigger command, A = Accent, 0-7 = triggers
+      next_pitches[receivedCommands[0] & 0b00000111] = receivedCommands[1];
+    else if (receivedCommands[0] & 0b01000000) // 0b10xxxaaa 0bvvvvvvvv  pitch change, aaa = address, v = value 
+      next_triggers = receivedCommands[1];
 
-	//------ Add current sample word to ringbuffer FIFO --------------------  
+    dataReady = false;
+  }
+}
 
-	if (RingCount<255) {  //if space in ringbuffer
-		total=0;
-		if (samplecntBD) {
-			phaccBD+=pitchBD;
-			if (phaccBD & 128) {
-				phaccBD &= 127;
-				samplepntBD++;
-				samplecntBD--;
+void process_audio() {
+  if (RingCount<255) {  //if space in ringbuffer
+    total=0;
+    if (samplecntBD) {
+      phaccBD+=pitchBD;
+      if (phaccBD & 128) {
+        phaccBD &= 127;
+        samplepntBD++;
+        samplecntBD--;
 
-			}
-			total+=(pgm_read_byte_near(BD + samplepntBD)-128);
-		}
-		if (samplecntSD) {
-			phaccSD+=pitchSD;
-			if (phaccSD & 128) {
-				phaccSD &= 127;
-				samplepntSD++;
-				samplecntSD--;
+      }
+      total+=(pgm_read_byte_near(BD + samplepntBD)-128);
+    }
+    if (samplecntSD) {
+      phaccSD+=pitchSD;
+      if (phaccSD & 128) {
+        phaccSD &= 127;
+        samplepntSD++;
+        samplecntSD--;
 
-			}
-			total+=(pgm_read_byte_near(SN + samplepntSD)-128);
-		}
-		if (samplecntCL) {
-			phaccCL+=pitchCL;
-			if (phaccCL & 128) {
-				phaccCL &= 127;
-				samplepntCL++;
-				samplecntCL--;
+      }
+      total+=(pgm_read_byte_near(SN + samplepntSD)-128);
+    }
+    if (samplecntCL) {
+      phaccCL+=pitchCL;
+      if (phaccCL & 128) {
+        phaccCL &= 127;
+        samplepntCL++;
+        samplecntCL--;
 
-			}
-			total+=(pgm_read_byte_near(CL + samplepntCL)-128);
-		}
-		if (samplecntRS) {
-			phaccRS+=pitchRS;
-			if (phaccRS & 128) {
-				phaccRS &= 127;
-				samplepntRS++;
-				samplecntRS--;
+      }
+      total+=(pgm_read_byte_near(CL + samplepntCL)-128);
+    }
+    if (samplecntRS) {
+      phaccRS+=pitchRS;
+      if (phaccRS & 128) {
+        phaccRS &= 127;
+        samplepntRS++;
+        samplecntRS--;
 
-			}
-			total+=(pgm_read_byte_near(RS + samplepntRS)-128);
-		}
-		if (samplecntCH) {
-			phaccCH+=pitchCH;
-			if (phaccCH & 128) {
-				phaccCH &= 127;
-				samplepntCH++;
-				samplecntCH--;
+      }
+      total+=(pgm_read_byte_near(RS + samplepntRS)-128);
+    }
+    if (samplecntCH) {
+      phaccCH+=pitchCH;
+      if (phaccCH & 128) {
+        phaccCH &= 127;
+        samplepntCH++;
+        samplecntCH--;
 
-			}
-			total+=(pgm_read_byte_near(CH + samplepntCH)-128);
-		}    
-		if (samplecntOH) {
-			phaccOH+=pitchOH;
-			if (phaccOH & 128) {
-				phaccOH &= 127;
-				samplepntOH++;
-				samplecntOH--;
+      }
+      total+=(pgm_read_byte_near(CH + samplepntCH)-128);
+    }    
+    if (samplecntOH) {
+      phaccOH+=pitchOH;
+      if (phaccOH & 128) {
+        phaccOH &= 127;
+        samplepntOH++;
+        samplecntOH--;
 
-			}
-			total+=(pgm_read_byte_near(OH + samplepntOH)-128);
-		}  
-		if (samplecntCR) {
-			phaccCR+=pitchCR;
-			if (phaccCR & 128) {
-				phaccCR &= 127;
-				samplepntCR++;
-				samplecntCR--;
+      }
+      total+=(pgm_read_byte_near(OH + samplepntOH)-128);
+    }  
+    if (samplecntCR) {
+      phaccCR+=pitchCR;
+      if (phaccCR & 128) {
+        phaccCR &= 127;
+        samplepntCR++;
+        samplecntCR--;
 
-			}
-			total+=(pgm_read_byte_near(CR + samplepntCR)-128);
-		}  
-		if (samplecntRD) {
-			phaccRD+=pitchRD;
-			if (phaccRD & 128) {
-				phaccRD &= 127;
-				samplepntRD++;
-				samplecntRD--;
+      }
+      total+=(pgm_read_byte_near(CR + samplepntCR)-128);
+    }  
+    if (samplecntRD) {
+      phaccRD+=pitchRD;
+      if (phaccRD & 128) {
+        phaccRD &= 127;
+        samplepntRD++;
+        samplecntRD--;
 
-			}
-			total+=(pgm_read_byte_near(RD + samplepntRD)-128);
-		}  
-		total>>=1;  
-		if (!(PINB&4)) total>>=1;
-		total+=128;  
-		if (total>255) total=255;
+      }
+      total+=(pgm_read_byte_near(RD + samplepntRD)-128);
+    }  
+    total>>=1;  
+    if (!(PINB&4)) total>>=1;
+    total+=128;  
+    if (total>255) total=255;
 
-		cli();
-		Ringbuffer[RingWrite]=total;
-		RingWrite++;
-		RingCount++;
-		sei();
-	}
+    cli();
+    Ringbuffer[RingWrite]=total;
+    RingWrite++;
+    RingCount++;
+    sei();
+  }
+}
 
-	//----------------------------------------------------------------------------
-
-	//----------------- Handle Triggers ------------------------------
-	if (Serial.available() > 0) {
-		ser = Serial.read();
-		Serial.write(ser);
-		switch (ser){
-			case('a'):	
-				samplepntBD=0;
-				samplecntBD=2154;
-				break;
-			case('s'): 
-				samplepntSD=0;
-				samplecntSD=3482;
-				break;
-			case('d'): 
-				samplepntCH=0;
-				samplecntCH=482;
-				break;
-			case('f'): 
-				samplepntOH=0;
-				samplecntOH=2572;
-				break;
-			case('j'): 
-				samplepntRS=0;
-				samplecntRS=1160;
-				break;
-			case('k'): 
-				samplepntCL=0;
-				samplecntCL=2384;
-				break;
-			case('l'): 
-				samplepntRD=0;
-				samplecntRD=5066;
-				break;
-			case(';'): 
-				samplepntCR=0;
-				samplecntCR=5414;
-				break;
-			default:
-				break;
-		}
-	}
-
-	//-----------------------------------------------------------------
+void loop() {  
+  process_audio();
+	process_i2c_data();
+  
+//  bool new_flip = digitalRead(2);
+//  if (new_flip != flip) {
+//    flip = !flip;
+//    
+//    updateParams();
+//  }
 }
